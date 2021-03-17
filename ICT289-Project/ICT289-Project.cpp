517438be2d50e19e95e49a1331e24afdfc99ff3c
Bryce Standley <bryce@vectorpixel.net>
2021-03-17 11:47:04 +0800

    Init VS Project

---
+++ ICT289-Project/ICT289-Project.cpp
@@ -0,0 +1,9 @@
+// ICT289-Project.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+#include<GL\freeglut.h>
+#include <iostream>
+
+int main()
+{
+    std::cout << "Hello World!\n";
+}
